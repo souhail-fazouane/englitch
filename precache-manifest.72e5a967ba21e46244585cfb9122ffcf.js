c66788cec51c8630362b0c73872ab63d1bf666e1
Fazouane Souhail <souhail.fazouane@telecomnancy.eu>
2021-09-02 16:38:48 +0200

    Updates

--- precache-manifest.72e5a967ba21e46244585cfb9122ffcf.js
+++ precache-manifest.72e5a967ba21e46244585cfb9122ffcf.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "93a9c83126f283e575ad6bf1ba4faddf",
+    "revision": "b0ce07a277297c934c48d6d9de8ca329",
     "url": "/englitch/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/englitch/static/css/2.69121389.chunk.css"
   },
   {
-    "revision": "3d1e9a088439736e24b6",
-    "url": "/englitch/static/css/main.75ba6b80.chunk.css"
+    "revision": "ac3aa887128f1f305230",
+    "url": "/englitch/static/css/main.0b9cf4d2.chunk.css"
   },
   {
     "revision": "3343cba2f20f7bcb4828",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/englitch/static/js/2.33d5a8f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3d1e9a088439736e24b6",
-    "url": "/englitch/static/js/main.f3c185d0.chunk.js"
+    "revision": "ac3aa887128f1f305230",
+    "url": "/englitch/static/js/main.5947c43d.chunk.js"
   },
   {
     "revision": "833c48dcf9b6632411f8",
